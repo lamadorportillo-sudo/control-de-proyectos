991fb93b6dbdb1789f1d5d32184f7e82fc3e5451
lamadorportillo-sudo <lamadorportillo@gmail.com>
2026-09-01 23:25:39 -0600

    Renovar cache para centro global de garantias

--- service-worker-v1.js
+++ service-worker-v1.js
@@ -1,4 +1,4 @@
-const CACHE='cc-static-v1-20260901-finance-center-v4c';
+const CACHE='cc-static-v1-20260901-control-centers-v4d';
 const STATIC_EXT=/\.(?:js|css|webp|png|jpg|jpeg|woff2?)(?:\?|$)/i;
 
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
